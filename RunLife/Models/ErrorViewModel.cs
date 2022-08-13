e5334fc35a07edaa537d81415a524d8795b958ec
Md Ariful Islam <arifulatwork@gmail.com>
2022-08-13 22:45:27 +0800

    Start with the project

---
+++ RunLife/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RunLife.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
